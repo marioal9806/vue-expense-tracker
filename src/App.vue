bfa15836344d7416163bd8c376208c76f5ece2af
Mario Ortega <marioal9806@gmail.com>
2024-01-15 23:29:49 -0600

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <Header></Header>
+  <div class="container">
+    <Balance></Balance>
+    <IncomeExpenses></IncomeExpenses>
+    <TransactionList :transactions="transactions"></TransactionList>
+    <AddTransaction></AddTransaction>
+  </div>
+</template>
+
+<script setup>
+import Header from './components/Header.vue';
+import Balance from './components/Balance.vue';
+import IncomeExpenses from './components/IncomeExpenses.vue';
+import TransactionList from './components/TransactionList.vue';
+import AddTransaction from './components/AddTransaction.vue';
+
+import { ref } from 'vue';
+
+const transactions = ref([
+  {id: 1, text: 'Flower', amount: -19.99},
+  {id: 2, text: 'Salary', amount: 299.97},
+  {id: 3, text: 'Book', amount: -10},
+  {id: 4, text: 'Camera', amount: 150},
+]);
+</script>
---
+++ src/components/TransactionList.vue
@@ -0,0 +1,21 @@
+<template>
+  <h3>History</h3>
+    <ul id="list" class="list">
+      <li 
+        v-for="transaction in transactions"
+        v-bind:key="transaction.id"
+        :class="transaction.amount < 0 ? 'minus':'plus'"
+      >
+        {{ transaction.text }} <span>{{ transaction.amount }}</span><button class="delete-btn">x</button>
+      </li>
+    </ul>
+</template>
+
+<script setup>
+const props = defineProps({
+  transactions: {
+    type: Array,
+    required: true,
+  }
+});
+</script>
